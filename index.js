e9d62223ce85b8f8127cc857ffa2c62207f64d59
adiadamomdad1147 <130950599+adiadamomdad1147@users.noreply.github.com>
2023-09-12 07:25:16 +0000

    let cons var

---
+++ index.js
@@ -0,0 +1,10 @@
+const accountId = 11471147
+let accountEmail = "singhadarsh1611@gmail.com"
+var accountPassword = "1336"
+accountCity = "Jaipur"
+
+accountEmail = "ninja1147@gmail.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
