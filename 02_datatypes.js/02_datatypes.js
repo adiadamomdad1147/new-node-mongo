fc9e4a301e4c98e73ab357e28019a8a9ed442c49
adiadamomdad1147 <130950599+adiadamomdad1147@users.noreply.github.com>
2023-09-30 12:05:40 +0000

    COnversion of datatypes

---
+++ 02_datatypes.js/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let score = "33abc"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33abc" => NaN
