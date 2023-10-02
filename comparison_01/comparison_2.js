ef4ead96417eb980fab8d9ee6dbf52b11e63a9ae
adiadamomdad1147 <130950599+adiadamomdad1147@users.noreply.github.com>
2023-10-02 11:00:49 +0000

    comparison summary

---
+++ comparison_01/comparison_2.js
@@ -0,0 +1,27 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+console.log(null > 1);
+console.log(null == 1);
+console.log(null <= 1);
+console.log(null <= 1);
+
+console.log(null == 1);
+console.log(null < 1);
+console.log(null > 1);
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 2345326352726353736n
+
