e9f267b4cb26eca88873c6f1a0aa9c2350f80bc8
adiadamomdad1147 <130950599+adiadamomdad1147@users.noreply.github.com>
2023-10-04 04:00:31 +0000

    stack and heap

---
+++ dataypes-summary/datatypes-summary.js
@@ -0,0 +1,18 @@
+let myYoutubeName = "adityasinghydot"
+
+let anothername = myYoutubeName
+anothername = "chaiorcode"
+
+console.log(myYoutubeName);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "aditya1147@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
